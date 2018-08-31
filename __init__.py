4d62ee2903d777436abe68a07c4bc61b0d847fcc
mcsoini <33124051+mcsoini@users.noreply.github.com>
2018-08-31 10:45:41 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 20:09:56 2018
+
+@author: mcsoini
+"""
+
+
+from storedisagg.storedisagg.compcalc import ComponentCalculator
+from storedisagg.storedisagg.storage_disaggregator import StDisaggregator
+
+from storedisagg.example.example_data import get_example_data_100
