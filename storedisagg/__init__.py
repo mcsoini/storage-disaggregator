1053316fafd60add97ce8a1c01bc527aa9b1c15e
user <you@example.com>
2019-04-28 14:31:39 +0200

    update

---
+++ storedisagg/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 20:09:56 2018
+
+@author: mcsoini
+"""
+
+from storedisagg.compcalc import ComponentCalculator
+from storedisagg.storagedisaggregator import StDisaggregator
+
+from storedisagg.example.example_data import get_example_data_100
+
