0a2984b8ccb8a8037c13ba3999f9956b9bbf46ba
user <you@example.com>
2019-04-28 14:31:10 +0200

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Ex-post energy storage operation disaggregation by time scales.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mcsoini/storedisagg,
+    url="https://github.com/mcsoini/storedisagg",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
